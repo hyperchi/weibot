a6e1498ad90566066b4f41795a4dcdfca82989a4
hyperchi <hyperbolechi@gmail.com>
2017-11-14 15:21:11 +0800

    fix h3

--- Application/static/js/components/Footer.js
+++ Application/static/js/components/Footer.js
@@ -29,8 +29,7 @@ const AppFooter = () => (
     <Grid style={style}>
       <Row>
         <Col xs={12} md={12}>
-          <h3 style={brandLabel}> Harvard Auto Schools(三友车行)
-          </h3>
+          <h3 style={brandLabel}> Harvard Auto Schools(三友车行)</h3>
         </Col>
       </Row>
       <Row>
